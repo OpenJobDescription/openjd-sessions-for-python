f34d3198586cf0bb2bd6c4ac1361619ce17d1a97
Robert Luttrell <roblutt@amazon.com>
2023-10-06 11:22:14 -0700

    Skip windows session user test on linux
    Signed-off-by: Robert Luttrell <roblutt@amazon.com>

--- test/openjd/sessions/test_session_user.py
+++ test/openjd/sessions/test_session_user.py
@@ -1,15 +1,18 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 from openjd.sessions._session_user import WindowsSessionUser
+from openjd.sessions._os_checker import is_windows
+
 import pytest
 
 
+@pytest.mark.skipif(not is_windows(), reason="Windows-specific tests")
 class TestWindowsSessionUser:
     @pytest.mark.parametrize(
         "user",
         ["userA", "domain\\userA"],
     )
-    def test_ctor_user_not_converted(self, user):
+    def test_user_not_converted(self, user):
         windows_session_user = WindowsSessionUser(user, group="test_group")
 
         assert windows_session_user.user == user
