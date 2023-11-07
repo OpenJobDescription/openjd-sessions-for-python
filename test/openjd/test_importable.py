475ea70524cae5b6b3ee6e9788dd060cc88ddcad
Daniel Neilson <53624638+ddneilson@users.noreply.github.com>
2023-11-07 17:47:52 +0000

    feat: export package version
    The package version is available in a private symbol. This adds the
    version value to the public interface of the library.
    
    Signed-off-by: Daniel Neilson <53624638+ddneilson@users.noreply.github.com>

--- test/openjd/test_importable.py
+++ test/openjd/test_importable.py
@@ -3,3 +3,11 @@
 
 def test_openjd_importable():
     import openjd  # noqa: F401
+
+
+def test_openjd_session_importable():
+    import openjd.sessions  # noqa: F401
+
+
+def test_version_importable():
+    from openjd.sessions import version  # noqa: F401
